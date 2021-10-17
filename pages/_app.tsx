f6c3ac6a9f080700ee5db8974a1e2748f7c4caf1
Sugar_F0x <sugarfox.dev@gmail.com>
2021-10-17 14:04:49 +0300

    Add redux store base

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { Provider } from 'react-redux';
+import type { AppProps } from 'next/app';
+import { store } from '../store';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({
+  Component, pageProps,
+}: AppProps) {
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
-export default MyApp
+
+export default MyApp;
---
+++ store/assets.ts
@@ -0,0 +1,24 @@
+import {
+  createSlice,
+  PayloadAction,
+} from '@reduxjs/toolkit';
+import type { RootState } from '.';
+
+export type AssetsState = {
+
+};
+
+const initialState: AssetsState = {
+
+};
+
+export const assetsSlice = createSlice({
+  name: 'assets',
+  initialState,
+  reducers: {
+
+  },
+});
+
+export const {
+} = assetsSlice.actions;
