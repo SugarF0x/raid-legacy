f2346b2562a774ccc6b30d03fe080612db3b50f0
Sugar_F0x <sugarfox.dev@gmail.com>
2021-10-17 23:53:00 +0300

    Add loading progress bar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,21 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 import { useAppDispatch, useAppSelector } from "../hooks"
-import { useEffect } from "react"
+import { useEffect, useMemo } from "react"
 import { initAssets } from "../store/assets/thunks"
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+import styles from './styles.module.css'
 
 const Home: NextPage = () => {
   const dispatch = useAppDispatch()
-  const assets = useAppSelector(state => state.assets)
+  const { entries, loadedEntries } = useAppSelector(state => state.assets)
 
   useEffect(() => {
     dispatch(initAssets())
   }, [])
 
-  useEffect(() => {
-    if ('loadedEntries' in assets && assets.entries === assets.loadedEntries) {
-      console.log(assets)
-    }
-  }, [assets])
+  const isLoading = useMemo(() => entries !== loadedEntries, [entries, loadedEntries])
 
   return (
     <div className={styles.container}>
@@ -29,58 +26,23 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        {isLoading ? (
+          <>
+            <h1>Loading assets...</h1>
+            <CircularProgressbar
+              value={loadedEntries || 0}
+              minValue={0}
+              maxValue={entries || 100}
+              text={`${loadedEntries || 0}/${entries || 0}`}
+              className={styles.progress}
+            />
+          </>
+        ) : (
+          <>
+            <h1>All assets loaded!</h1>
+          </>
+        )}
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   )
 }
