92cd23dfc0b6999b58354964f7a1795feb52bd80
Sugar_F0x <sugarfox.dev@gmail.com>
2021-10-18 21:53:30 +0300

    Refactor assets loading into dedicated component

---
+++ components/AssetsLoader.tsx
@@ -0,0 +1,37 @@
+import { useAppDispatch, useAppSelector } from "../hooks"
+import { ReactNode, useEffect, useMemo } from "react"
+import { initAssets } from "../store/assets/thunks"
+import styles from "../styles/AssetsLoader.module.css"
+import { CircularProgressbar } from "react-circular-progressbar"
+
+export function AssetsLoader(props: { children: ReactNode }) {
+  const { children } = props
+  const dispatch = useAppDispatch()
+  const { entries, loadedEntries } = useAppSelector(state => state.assets)
+
+  useEffect(() => {
+    dispatch(initAssets())
+  }, [])
+
+  const isLoading = useMemo(() => entries !== loadedEntries, [entries, loadedEntries])
+
+  return (
+    <>
+      {isLoading ? (
+        <div className={styles.wrapper}>
+          <CircularProgressbar
+            value={loadedEntries || 0}
+            minValue={0}
+            maxValue={entries || 100}
+            text={`${loadedEntries || 0}/${entries || 0}`}
+            className={styles.progress}
+          />
+        </div>
+      ) : (
+        <>
+          {children}
+        </>
+      )}
+    </>
+  )
+}
